befb860fb35fe5c87a9242f65fb065b84b60c4e7
pryhunova <prygunova@gmail.com>
2022-03-17 22:09:47 +0200

    Взаимодействие с пользователем

---
+++ index.js
@@ -0,0 +1,7 @@
+const greetings = "Приветствую!";
+alert(greetings);
+
+let userName = prompt("Представьтесь, пожалуйста:");
+let userAge = prompt("Укажите Ваш возраст:");
+
+alert(`Добро пожаловать, ${userName}. Ваш возраст: ${userAge}`);
