cc804bba9f00bf91a81880ff25674163cb6a1343
pryhunova <prygunova@gmail.com>
2022-03-21 14:50:42 +0200

    Добавляет вариант Андрея

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ if (!parseInt(userAge)) {
 } else if (userName.trim().length && userAge.trim().length > 0) {
   alert(`Добро пожаловать, ${userName}. Ваш возраст: ${userAge}`);
 }
+
+// const greetingMessage = 'Здоровенькі були!';
+
+// alert(greetingMessage);
+
+// let userName = prompt('Як вас звати?');
+// let userAge = parseInt(prompt('Cкільки вам років?'));
+
+// if (userName.trim().length && userAge) {
+//   console.log(`Вітаю пане ${userName}. Вам ${userAge} років!`);
+// } else if (Number.isNaN(userAge)) {
+//   alert('Вкажіть будь-ласка вік числами!');
+// } else if (!userName.trim().length) {
+//   alert('Введіть хоча б першу літеру вашого імені!');
+// }
