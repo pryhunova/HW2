abb50c50c3934f0f2eb98f0316731f164524508f
pryhunova <prygunova@gmail.com>
2022-03-27 20:14:47 +0300

    Добавила количество операций

--- js/hw-3.js
+++ js/hw-3.js
@@ -1,38 +1,45 @@
 'use strict';
+function calculator() {
+  let quantity = +prompt('Укажите количество операций');
 
-let firstOperand = +prompt('Введите первое число');
-let operator = prompt('Введите один из операторов: "- + * /"');
-let secondOperand = +prompt('Введите второе число');
-let result;
+  for (let index = 0; index < quantity; index++) {
+    let firstOperand = +prompt('Введите первое число');
+    let operator = prompt('Введите один из операторов: "- + * /"');
+    let secondOperand = +prompt('Введите второе число');
+    let result;
 
-// проверяет условие и показывает результат операции
-function calculatorResult() {
-  if (firstOperand && secondOperand) {
-    alert(`Результат: ${firstOperand}${operator}${secondOperand}= ${result}`);
-  } else if (Number.isNaN(firstOperand) || Number.isNaN(secondOperand)) {
-    alert('Используйте только числа!');
+    // проверяет условие и показывает результат операции
+    function calculatorResult() {
+      if (firstOperand && secondOperand) {
+        alert(
+          `Результат: ${firstOperand}${operator}${secondOperand}= ${result}`,
+        );
+      } else if (Number.isNaN(firstOperand) || Number.isNaN(secondOperand)) {
+        alert('Используйте только числа!');
+      }
+    }
+    operatorChecks();
+    //проверяет оператор
+    function operatorChecks() {
+      switch (operator) {
+        case '+':
+          result = firstOperand + secondOperand;
+          break;
+        case '-':
+          result = firstOperand - secondOperand;
+          break;
+        case '*':
+          result = firstOperand * secondOperand;
+          break;
+        case '/':
+          result = firstOperand / secondOperand;
+          break;
+        default:
+          alert(`Мы не можем выполнить действие с оператором ${operator}`);
+      }
+      return calculatorResult();
+    }
   }
 }
 
-//проверяет оператор
-function operatorChecks() {
-  switch (operator) {
-    case '+':
-      result = firstOperand + secondOperand;
-      break;
-    case '-':
-      result = firstOperand - secondOperand;
-      break;
-    case '*':
-      result = firstOperand * secondOperand;
-      break;
-    case '/':
-      result = firstOperand / secondOperand;
-      break;
-    default:
-      alert(`Мы не можем выполнить действие с оператором ${operator}`);
-  }
-  return calculatorResult();
-}
-
-operatorChecks();
+calculator();
