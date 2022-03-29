ec024c94b2695ebb4c90131a540b34447d97d849
pryhunova <prygunova@gmail.com>
2022-03-30 00:00:19 +0300

    Задача 5

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,36 +1,38 @@
 'use strict';
 
 const movies = [
-  { id: 1, name: 'Fast and Furious', cost: 5.99 },
-  { id: 2, name: 'Fast and Furious 2', cost: 3.99 },
-  { id: 3, name: 'Fast and Furious 4', cost: 4.99 },
-  { id: 4, name: 'Shrek', cost: 12.99 },
+  { id: 1, name: 'Shutter Island', cost: 5.99 },
+  { id: 2, name: 'Deadpool', cost: 4.99 },
+  { id: 3, name: 'Deadpool 2', cost: 4.99 },
+  { id: 4, name: 'Alien', cost: 13.99 },
+  { id: 5, name: 'The Shawshank Redemption', cost: 4.99 },
+  { id: 6, name: 'Inception', cost: 4.99 },
+  { id: 7, name: 'Fight Club', cost: 5.99 },
+  { id: 8, name: 'The Matrix', cost: 13.99 },
+  { id: 9, name: 'Interstellar', cost: 4.99 },
+  { id: 10, name: 'The Silence of the Lambs', cost: 4.99 },
 ];
 
 const cart = [];
 
 function addToCart(id) {
-  cart.push(movies.id);
-  return cart;
+  const movie = movies.find(movie => movie.id === id);
+  return cart.push(movie);
 }
 
 function printCheck() {
-  // your solution here...
+  cart.map((item, i) => {
+    console.log(`${i + 1} ${item.name} цена: ${item.cost}`);
+  });
+}
+
+function getTotalPrice() {
+  const totalPrice = cart.reduce((total, { cost }) => total + cost, 0);
+  console.log(`Итого: ${totalPrice}`);
 }
 
 addToCart(2);
-console.log(addToCart());
-addToCart(4);
-
+addToCart(5);
+addToCart(8);
 printCheck();
-
-/*Вам нужно будет реализовать две функции для магазина фильмов напрокат: 
-одну для добавления товаров в корзину(она должна называться addToCart) и еще одну для формирования чека(printCheck).
-
-Нужно чтобы при вызове функции addToCart в неё можно было передать уникальный идентификатор
-(или по - простому id - шник) фильма.После чего фильм с таким айдишником должен должен попасть в массив товаров с корзиной.
-
-Функция printCheck должна просто вывести всё что есть в корзине в консоль, без id, но с порядковым номером.
-
-Выходной формат должен быть следующим:
-№ -- Название фильма -- $цена */
+getTotalPrice();
