6587088512e18b112878cc161b14a36cc09e277f
pryhunova <prygunova@gmail.com>
2022-04-04 01:31:27 +0300

    Проверка чётных и простых чисел

--- js/hw-2.js
+++ js/hw-2.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// const greetingMessage = 'Здоровенькі були!';
+const greetingMessage = 'Здоровенькі були!';
 
-// alert(greetingMessage);
+alert(greetingMessage);
 
-// let userName = prompt('Як вас звати?');
-// let userAge = parseInt(prompt('Cкільки вам років?'));
+let userName = prompt('Як вас звати?');
+let userAge = parseInt(prompt('Cкільки вам років?'));
 
-// if (userName.trim().length && userAge) {
-//   console.log(`Вітаю пане ${userName}. Вам ${userAge} років!`);
-// } else if (Number.isNaN(userAge)) {
-//   alert('Вкажіть будь-ласка вік числами!');
-// } else if (!userName.trim().length) {
-//   alert('Введіть хоча б першу літеру вашого імені!');
-// }
+if (userName.trim().length && userAge) {
+  console.log(`Вітаю пане ${userName}. Вам ${userAge} років!`);
+} else if (Number.isNaN(userAge)) {
+  alert('Вкажіть будь-ласка вік числами!');
+} else if (!userName.trim().length) {
+  alert('Введіть хоча б першу літеру вашого імені!');
+}
--- js/hw-6.js
+++ js/hw-6.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+  let question = +prompt('Введите число');
+
+  if (Number.isNaN(question)) {
+    alert('Используйте только числа!');
+  } else {
+    isEven();
+    isPrime();
+  }
+  function isEven() {
+    if (question % 2 == 0) {
+      alert('Вы ввели чётное число');
+    }
+  }
+
+  function isPrime() {
+    for (let i = 2; i < question; i++) {
+      if (question % i === 0) return false;
+    }
+    return alert('Вы ввели простое число');
+  }
+});
