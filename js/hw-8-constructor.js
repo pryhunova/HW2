072784170731b50fc9e3e0a5798866260a7dd68f
pryhunova <prygunova@gmail.com>
2022-04-11 20:08:08 +0300

    Update hw-8-constructor.js

--- js/hw-8-constructor.js
+++ js/hw-8-constructor.js
@@ -1,9 +1,11 @@
+const buttonRef = document.querySelector('#btn');
 const hotelRef = document.querySelector('#hotel');
 
-const hotelConfig = {
-  accommodation: ['Room', ' Apartments', ' Cottage'],
-  transfer: ['Yes', ' No'],
-  breakfast: ['Yes', ' No'],
+const orderList = [];
+const orderConfig = {
+  accommodation: ['Room', 'Apartments', 'Cottage'],
+  transfer: ['Yes', 'No'],
+  breakfast: ['Yes', 'No'],
 };
 
 const Hotel = {
@@ -11,50 +13,55 @@ const Hotel = {
     this.hotelName = hotelName;
     this.capacity = capacity;
 
-    greet = function (guestName) {
-      guestName = prompt('Please, enter your name:', '');
-      hotelRef.insertAdjacentHTML(
-        'beforeend',
-        `<p>Hello ${guestName}, wellcome to ${hotelName}.</p>`,
-      );
-    };
-    greet();
-
-    createAnOrder = function () {
-      const order = {};
-      for (let option in hotelConfig) {
-        order[option] = prompt(
-          `Please, choose the options ${option}: ${hotelConfig[option]}.`,
-          '',
-        );
+    buttonRef.addEventListener('click', () => {
+      greet = function (guestName) {
+        guestName = prompt('Please, enter your name:', '');
         hotelRef.insertAdjacentHTML(
           'beforeend',
-          `<p>${option}: ${order[option]}<p>`,
+          `<p>Hello, ${guestName}! Wellcome to ${hotelName}.</p>`,
         );
-      }
-      return order;
-    };
+        orderList.push(guestName);
+        return;
+      };
+      greet();
 
-    addGuests = function (amount) {
-      amount = +prompt('Please, enter the amount of guests.', '');
+      createAnOrder = function () {
+        const order = {};
+        for (let option in orderConfig) {
+          order[option] = prompt(
+            `Please, choose the options ${option}: ${orderConfig[option]}.`,
+            '',
+          );
+          hotelRef.insertAdjacentHTML(
+            'beforeend',
+            `<p>${option}: ${order[option]}<p>`,
+          );
+        }
+        return orderList.push(order);
+      };
 
-      if (amount > capacity) {
-        hotelRef.insertAdjacentHTML(
-          'beforeend',
-          `<p>Sorry, we don't have ${amount} places. There are ${capacity} available places in ${hotelName}.</p>`,
-        );
-      } else {
-        capacity -= amount;
-        hotelRef.insertAdjacentHTML(
-          'beforeend',
-          `<p>Amount of guests: ${amount}. There are ${capacity} available places in ${hotelName}.</p>`,
-        );
-        createAnOrder();
-      }
-    };
-    addGuests();
+      addGuests = function (amount) {
+        amount = +prompt('Please, enter the amount of guests.', '');
+        if (amount > capacity) {
+          hotelRef.insertAdjacentHTML(
+            'beforeend',
+            `<p>Sorry, we don't have ${amount} places. There are ${capacity} available places in ${hotelName}.</p>`,
+          );
+          orderConfig.push(amount, capacity);
+        } else {
+          capacity -= amount;
+          hotelRef.insertAdjacentHTML(
+            'beforeend',
+            `<p>Amount of guests: ${amount}. There are ${capacity} available places in ${hotelName}.</p>`,
+          );
+          createAnOrder();
+          return orderList.push(amount, capacity);
+        }
+      };
+      addGuests();
+    });
+    console.log(orderList);
   },
 };
 
 const hotel = Object.create(Hotel).constructor('Odesa Hotel', 50);
-// const hote2 = Object.create(Hotel).constructor('Uman Hotel', 25);
