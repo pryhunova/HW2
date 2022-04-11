9071a9c1a67ccf4655b6eb8786dd76be39ed6c7f
pryhunova <prygunova@gmail.com>
2022-04-11 12:11:40 +0300

    Update hw-10-chunk.js

--- js/hw-10-chunk.js
+++ js/hw-10-chunk.js
@@ -3,13 +3,13 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function chunk(list, chunkSize) {
   let chunks = [];
 
-  for (let i = 0; i < list.length; i += chunkSize) {
-    if (!chunkSize) {
-      return `Error`;
-    }
-    if (chunkSize === 1) {
-      return list;
-    } else {
+  if (!chunkSize) {
+    return `Error`;
+  }
+  if (chunkSize === 1) {
+    return list;
+  } else {
+    for (let i = 0; i < list.length; i += chunkSize) {
       chunks.push(list.slice(i, i + chunkSize));
     }
   }
@@ -23,8 +23,8 @@ console.log(result); // [[1,2,3], [4,5,6], [7,8,9], [10]]
 const result2 = chunk(arr, 5); // 5 - размер каждого кусочка
 console.log(result2); // [[1,2,3,4,5], [6,7,8,9,10]]
 
-// const result = chunk(arr, 1); // 1 - размер каждого кусочка
-// console.log(result); // [1,2,3,4,5,6,7,8,9,10] - то есть исходный массив
+const result4 = chunk(arr, 1); // 1 - размер каждого кусочка
+console.log(result4); // [1,2,3,4,5,6,7,8,9,10] - то есть исходный массив
 
-// const result = chunk(arr, 0); // 0 - размер каждого кусочка
-// console.log(result); // написать пользователю в консоль что он не прав
+const result3 = chunk(arr, 0); // 0 - размер каждого кусочка
+console.log(result3); // написать пользователю в консоль что он не прав
