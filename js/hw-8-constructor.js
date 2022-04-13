96b8c5350d4b31bb2f301faced9166305fc6a0a6
pryhunova <prygunova@gmail.com>
2022-04-13 12:25:17 +0300

    Update hw-8

--- js/hw-8-constructor.js
+++ js/hw-8-constructor.js
@@ -1,9 +1,11 @@
 'use strict';
 const buttonRef = document.querySelector('#btn');
 const hotelRef = document.querySelector('#hotel');
+const orderOptionsRef = document.querySelector('#order-options');
 
 const hotel = {
   hotelName: 'Odesa Hotel',
+  capacity: 50,
   orderList: [],
   orderConfig: {
     accommodation: ['Room', 'Apartments', 'Cottage'],
@@ -17,23 +19,40 @@ buttonRef.addEventListener('click', () => {
   const amount = +prompt('Please, enter the amount of guests.', '');
   const order = {};
 
-  for (let option in hotel.orderConfig) {
-    if (hotel.orderConfig.hasOwnProperty(option)) {
+  function addOrderToHTML() {
+    hotelRef.insertAdjacentHTML(
+      'beforebegin',
+      `<h2>Hello, ${guestName}! Wellcome to ${hotel.hotelName}.</h2>`,
+    );
+    if (amount > hotel.capacity) {
+      hotelRef.insertAdjacentHTML(
+        'beforebegin',
+        `<p>Sorry, we don't have ${amount} places. There are ${hotel.capacity} available places in ${hotel.hotelName}.</p>`,
+      );
+    } else {
+      hotel.capacity -= amount;
+      hotelRef.insertAdjacentHTML(
+        'beforebegin',
+        `<p>Amount of guests: ${amount}. There are ${hotel.capacity} available places in ${hotel.hotelName}.</p>`,
+      );
+      chooseOrderOptions();
+    }
+  }
+
+  function chooseOrderOptions() {
+    for (let option in hotel.orderConfig) {
       order[option] = prompt(
         `Please, choose the options ${option}: ${hotel.orderConfig[option]}.`,
         '',
       );
+      orderOptionsRef.insertAdjacentHTML(
+        'beforeend',
+        `<li>${option}: ${order[option]}</li>`,
+      );
     }
   }
-  hotel.orderList.push(guestName, amount, order);
+  hotel.orderList.push(guestName, amount, hotel.capacity, order);
+  addOrderToHTML();
 });
 
 console.log(hotel.orderList);
-
-function addOrderToHTML() {
-  hotelRef.insertAdjacentHTML(
-    'beforeend',
-    `<p>Hello, ${guestName}! Wellcome to ${hotel.hotelName}.</p><p>Amount of guests: ${amount}</p><p>Details of your order. ${option}: ${order[option]}<p>`,
-  );
-}
-addOrderToHTML();
