18427a719a4b605e4bff5c95e294b5767f828e96
pryhunova <prygunova@gmail.com>
2022-05-19 14:36:58 +0300

    HW-16

--- js/hw-16-TodoList.js
+++ js/hw-16-TodoList.js
@@ -1,30 +1,72 @@
+const containerRef = document.querySelector('.container');
+
+const todoContainerEl = document.createElement('div');
+const todoTitleEl = document.createElement('h1');
+const todoInputEl = document.createElement('input');
+const todoBtnEl = document.createElement('button');
+const todoListEl = document.createElement('ul');
+
+todoTitleEl.textContent = 'Список завдань';
+todoInputEl.placeholder = 'Нове завдання';
+todoBtnEl.textContent = 'Додати';
+
+todoContainerEl.classList.add('todo-container');
+todoTitleEl.classList.add('todo-title');
+todoInputEl.classList.add('todo-input');
+todoBtnEl.classList.add('todo-button');
+todoListEl.classList.add('todos');
+
+containerRef.append(todoContainerEl);
+todoContainerEl.append(todoTitleEl, todoInputEl, todoBtnEl, todoListEl);
+
+todoBtnEl.addEventListener('click', () => {
+  if (todoInputEl.value !== '') {
+    const todoItem = document.createElement('li');
+    const todoCheckbox = document.createElement('input');
+    const inpChecked = document.createElement('label');
+
+    todoCheckbox.type = 'checkbox';
+    todoItem.classList.add('todo-item');
+    todoCheckbox.classList.add('todo-checkbox');
+
+    todoItem.innerText = todoInputEl.value;
+    newTodoList.add({ title: todoInputEl.value, done: false });
+
+    console.log(newTodoList);
+
+    todoListEl.append(todoItem);
+    todoItem.append(todoCheckbox, inpChecked);
+
+    todoCheckbox.addEventListener('click', () => {
+      if (todoCheckbox.checked == true) {
+        todoItem.style.textDecoration = 'line-through';
+      } else {
+        todoItem.style.textDecoration = 'none';
+      }
+    });
+  }
+  todoInputEl.value = '';
+});
+
 class TodoList {
   items = [];
 
-  add(id, title, description, done) {
-    const newTodoItem = new TodoItem(id, title, description, done);
+  add({ title, done }) {
+    const newTodoItem = new TodoItem({ title, done });
     this.items.push(newTodoItem);
   }
-
   completeAll() {
     this.items.forEach(item => (item.done = true));
   }
 }
 
 class TodoItem {
-  constructor(id, title, description, done) {
-    this.id = id;
+  constructor({ title, done }) {
     this.title = title;
-    this.description = description;
     this.done = done;
   }
 }
 
 const newTodoList = new TodoList();
 
-newTodoList.add(1, 'HTML', 'Website structure', true);
-newTodoList.add(2, 'CSS', 'Website styles', true);
-newTodoList.add(3, 'JS', 'Website interactivity', true);
 newTodoList.completeAll();
-
-console.log(newTodoList);
