5ef7daf5ec96c2af2e565d81571826e1503745b1
pryhunova <prygunova@gmail.com>
2022-06-13 15:07:57 +0300

    Добавляет ДЗ-21 XMLHttpRequest

--- js/hw-14-calculator.js
+++ js/hw-14-calculator.js
@@ -1,12 +1,68 @@
-const display = document.getElementById('display');
-console.log(display);
-const buttons = Array.from(document.getElementsByClassName('calculator__btn'));
+const refs = {
+  numberButtons: document.querySelectorAll('[data-number]'),
+  operationButtons: document.querySelectorAll('[data-operation]'),
+  equalsButton: document.querySelector('[data-equals]'),
+  allClearButton: document.querySelector('[data-all-clear]'),
+  // previousOperandTextElement: document.querySelector('[data-previous-operand]'),
+  currentValue: document.querySelector('[data-current-operand]'),
+};
 
-buttons.map(button => {
-  button.addEventListener('click', e => {
-    switch (e.target.innerText) {
-      default:
-        display.innerText += e.target.innerText;
+let a = '';
+let b = '';
+let calculationSign = '';
+let result = false;
+
+const clearAll = () => {
+  a = '';
+  b = '';
+  calculationSign = '';
+  result = false;
+  refs.currentValue.textContent = '0';
+};
+
+refs.allClearButton.addEventListener('click', () => {
+  clearAll();
+});
+
+refs.numberButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    refs.currentValue.textContent = '';
+
+    if (b === '' && calculationSign === '') {
+      a += event.target.textContent;
+      refs.currentValue.textContent = a;
+    } else if (a !== '' && b !== '' && result) {
+    } else {
+      b += event.target.textContent;
+      refs.currentValue.textContent = b;
     }
   });
+});
+
+refs.operationButtons.forEach(operator => {
+  operator.addEventListener('click', event => {
+    calculationSign += event.target.textContent;
+    refs.currentValue.textContent = calculationSign;
+    console.log(a, b, calculationSign);
+  });
+});
+
+refs.equalsButton.addEventListener('click', e => {
+  switch (e) {
+    case '+':
+      a = a + b;
+      break;
+    case '-':
+      a = a - b;
+      break;
+    case 'x':
+      a = a * b;
+      break;
+    case '/':
+      a = a / b;
+      break;
+  }
+  result = true;
+  refs.currentValue.textContent = a;
+  console.log(a, b, calculationSign);
 });
---
+++ js/hw-21-XMLHttpRequest.js
@@ -0,0 +1,25 @@
+/* Getting a resource */
+const xhrGET = new XMLHttpRequest();
+xhrGET.open('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+xhrGET.send();
+
+xhrGET.onload = function () {
+  if (xhrGET.status != 200) {
+    console.log(`Error: + ${xhrGET.status}`);
+  } else {
+    console.log(`It's done, we got: ${xhrGET.response}`);
+  }
+};
+
+/* Creating a resource */
+const xhrPOST = new XMLHttpRequest();
+const json = JSON.stringify({
+  name: 'Natalie',
+  surname: 'Pryhunova',
+});
+
+xhrPOST.open('POST', 'https://jsonplaceholder.typicode.com/posts/');
+xhrPOST.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+xhrPOST.send(json);
+xhrPOST.onload = () => console.log(xhrPOST.response);
